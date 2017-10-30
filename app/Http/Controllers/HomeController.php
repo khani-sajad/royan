4ce13c7c1280ac04f0cf641f19cf7ac626f91f75
ali <ali.seyfi68@gmail.com>
2017-10-30 17:22:42 +0330

    Bug 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth')->exept('forms');
+        $this->middleware('auth')->except('forms');
     }
 
     public function index()
